9b0f72e038c965fadf948bbbd88bdec5a3259668
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2023-06-27 20:04:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+
+let name = document.querySelector("#name").value;
+let year = document.querySelector("#year").value;
+let ans = document.querySelector("span");
+
+let message = `?name=${name}&year=${year}`;
+let submit = document.querySelector("#button");
+submit.addEventListener('click',function(e){
+	e.preventDefault();
+	ans.innerText = message
+})
